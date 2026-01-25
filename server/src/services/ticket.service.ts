37a769dcb6d59b2cb336fb4a6e94b2723a7f5007
Claudio <ccmf.dev@gmail.com>
2026-01-25 11:35:30 -0300

    security(ticket): prevent overselling and validate ticket range

--- server/src/services/ticket.service.ts
+++ server/src/services/ticket.service.ts
@@ -7,24 +7,37 @@ const prisma = new PrismaClient();
 // Function to buy a ticket of a specific raffle
 export const buyTicket = async (data: {
     raffleId: string;
-    userId?: string;
-    guestEmail?: string;
+    userId?: string | null;
+    guestEmail?: string | null;
     numberValue: number;
 }) => {
 
-    // We created random operationId;
-    const operationId = `T-${generateShortCode()}`;
     // First, we check if the raffle exists and get its maxTickets
     const raffle = await prisma.raffle.findUnique({
-        where: { id: data.raffleId}
+        where: { id: data.raffleId},
+        include:{
+            _count:{
+                select:{
+                    tickets: true
+                } 
+            }
+        }
     });
 
     if (!raffle) throw new Error('Raffle not found');
 
+    if (raffle._count.tickets >= raffle.maxTickets) {
+        throw new Error('Agotado! Este sorteo alcanzó el máximo de números vendidos');
+    }
+
     // Check if the number is withinn the allowed range
     if(data.numberValue < 1 || data.numberValue > raffle.maxTickets){
-        throw new Error(`Numbers must be between 1 and ${raffle.maxTickets}`)
+        throw new Error(`Número inválido! Por favor ingresar entre 1 y ${raffle.maxTickets}`);
     }
+
+    // We created random operationId;
+    const operationId = `T-${generateShortCode()}`;
+
 
     return await prisma.ticket.create({
         data: {

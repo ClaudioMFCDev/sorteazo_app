a75e80e407992443aa53af1f92f2226a33f29c07
Claudio <ccmf.dev@gmail.com>
2026-01-27 11:23:50 -0300

    feat: setup react-router-dom and implement smart navigation links

--- client/src/components/layout/BottomNav.tsx
+++ client/src/components/layout/BottomNav.tsx
@@ -1,25 +1,38 @@
+import { NavLink } from 'react-router-dom';
 import { Home, Ticket, User, PlusCircle } from 'lucide-react';
+
 
 //Bottom navigation for mobile-first experience
 export const Bottom = () => {
+
+    // Utility function to handle anctive/inactive styles
+    // If isActive is true (provided by NavLink), we return blue. otherwise gray
+    const navItemStyles = ({ isActive }: { isActive: boolean}) => 
+        `flex flex-col items-center transition-colors ${isActive ? 'text-primary' : 'text-gray-400'}`;
+
     return(
         <nav className='fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 px-6 py-3 flex justify-between items-center z-50'>
-            <button className='flex flex-col items-center text-primary'>
+
+            {/* NavLink monitors the Url and changes its style automatically */}
+            <NavLink to='/' className={navItemStyles}>
                 <Home size={24}/>
                 <span className='text-xs mt-1'>Inicio</span>
-            </button>
-            <button className='flex flex-col items-center text-gray-400'>
+            </NavLink>
+            <NavLink to='/mis-rifas' className={navItemStyles}>
                 <Ticket size={24}/>
                 <span className='text-xs mt-1'>Mis Rifas</span>
-            </button>
-            <button className='flex flex-col items-center text-gray-400'>
-                <PlusCircle size={40} className='text-primary -mt-6 bg-white rounded-full border-4 border-white'/>
+            </NavLink>
+            {/* For the main action button, we might want a different logic, but let's keep simple for now */}
+            <NavLink to='/crear' className={navItemStyles}>
+                <div>
+                    <PlusCircle size={48} className='text-primary -mt-8 bg-white rounded-full border-4 border-white shadow-lg'/>
+                </div>
                 <span className='text-xs mt-1'>Crear</span>
-            </button>
-            <button className='flex flex-col items-center text-gray-400'>
+            </NavLink>
+            <NavLink to='/perfil' className={navItemStyles}>
                 <User size={24}/>
                 <span className='text-xs mt-1'>Perfil</span>
-            </button>
+            </NavLink>
         </nav>
     );
 };
